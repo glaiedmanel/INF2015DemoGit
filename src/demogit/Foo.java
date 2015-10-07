cddae94d78151e70e0744400b39ef9fbba3ceb64
db591164 <db591164@C39396.ens.uqam.ca>
2015-10-07 12:10:25 -0300

    corriger code class foo

---
+++ src/demogit/Foo.java
@@ -0,0 +1,17 @@
+package demogit;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author db591164
+ */
+public class Foo {
+    public void foo(){
+    System.out.println("hello there");
+    }
+}
